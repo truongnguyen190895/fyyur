716c55870422c83c6425324a8fc0f5ba4b3cb599
truong <ntruong@switch.tv>
2023-09-16 15:32:05 +0700

    implement edit artist

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify, abort
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -373,26 +373,54 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+
+  artist = Artist.query.get(artist_id)
+
+  if not artist:
+    # Return a 404 error if the artist doesn't exist
+    return abort(404)
+
+  form.name.data = artist.name
+  form.genres.data = artist.genres.split(', ')
+  form.city.data = artist.city
+  form.state.data = artist.state
+  form.phone.data = artist.phone
+  form.website_link.data = artist.website
+  form.facebook_link.data = artist.facebook_link
+  form.seeking_venue.data = artist.seeking_venue
+  form.seeking_description.data = artist.seeking_description
+  form.image_link.data = artist.image_link
+
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
+  try:
+    artist = Artist.query.get(artist_id)
+
+    if not artist:
+        # Handle the case where the artist does not exist
+        return abort(404)
+
+    artist.name = request.form.get('name')
+    artist.genres = ', '.join(request.form.getlist('genres'))  
+    artist.city = request.form.get('city')
+    artist.state = request.form.get('state')
+    artist.phone = request.form.get('phone')
+    artist.website = request.form.get('website_link')
+    artist.facebook_link = request.form.get('facebook_link')
+    artist.seeking_venue = request.form.get('seeking_venue') == 'y'
+    artist.seeking_description = request.form.get('seeking_description')
+    artist.image_link = request.form.get('image_link')
+
+    db.session.commit()
+
+  except Exception as error:
+    db.session.rollback()
+    flash('An error occurred. Artist ' + artist.name + ' could not be updated. Error message: ' + str(error), 'error')
+
+  finally:
+    db.session.close()
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
